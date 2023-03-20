cf79071006c8d483e97417093c6925b5dab1bca7
Peter <storm.sk123@gmail.com>
2023-03-20 22:52:02 +0100

    header is now working

--- resources/index.js
+++ resources/index.js
@@ -1,5 +1,10 @@
-// Change the text between the quotation marks and run your code to see the name of your favorite language animated!
-drawText('Bonjour le monde');
+var scrollTrigger = 60;
 
-// The code below will allow you to interact with the letters
-bounceBubbles();
+window.onscroll = function() {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add('scrolled');
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove('scrolled');
+  }
+};
