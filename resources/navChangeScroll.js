05fc9220956ac64b8f8ffe035e65c5c43038cba2
Nataly <117062171+itisnatalie@users.noreply.github.com>
2023-03-21 09:34:38 +0100

    Additional commit

---
+++ resources/navChangeScroll.js
@@ -0,0 +1,31 @@
+const header = document.querySelector("nav");
+const sectionOne = document.querySelector(".hero");
+
+// const sectionOneOptions = {
+//     rootMargin: "-200px 0px 0px 0px"
+// };
+
+// const sectionOneObserver = new IntersectionObserver(function(
+//     entries, 
+//     sectionOneObserver) {
+
+//      entries.forEach(entry => {
+//         if (!entry.isIntersecting) {
+//             header.classList.add("nav-scrolled");
+//         } else {
+//             header.classList.remove("nav-scrolled");
+//         }
+//      })   
+// }, sectionOneOptions)
+
+// sectionOneObserver.observe(sectionOne);
+var scrollTrigger = 175;
+window.onscroll = function() {
+    // We add pageYOffset for compatibility with IE.
+    if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+      header.classList.add("nav-scrolled");
+      console.log('working')
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  };
